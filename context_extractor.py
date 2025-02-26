3b4a9505233a1be16d72c0cac7f8ae1348cfb0c6
ItIsMeMyselfAndI <egermirasol2023@gmail.com>
2025-02-27 03:48:05 +0800

    created contex_extractor.py. refactored scripts. added token usage info.

---
+++ context_extractor.py
@@ -0,0 +1,128 @@
+from docx import Document
+import requests
+import json
+import os
+import time
+import winsound
+
+def extractContext(messages, chap):
+    url="https://openrouter.ai/api/v1/chat/completions"
+    # key = "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"
+    # model = "deepseek/deepseek-r1:free"
+    key = "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"
+    model="deepseek/deepseek-chat:free"
+        
+    for _ in range(10):
+        response = requests.post(
+            url = url,
+            headers = {
+                "Authorization": f"Bearer {key}",
+                "Content-Type": "application/json",
+            },
+            data = json.dumps({
+                "model": model,
+                "messages": messages
+            })
+        )
+        new_context = ""
+        response_json = response.json()
+        if "choices" in response_json.keys():
+            new_context = response_json["choices"][0]["message"]["content"].strip()
+            if ('{' in new_context) and '}' in new_context:
+                print(f"[*] Successful extraction - {chap}")
+                _displayUsageInfo(response_json)
+                return f"Chapter {chap}: {new_context}"
+            print(f"[!] Error: Incomplete extraction - {chap} >> Restarting ...")
+        else:
+            print(f"[!] Error: No response - {chap} >> Restarting ...")
+        _displayUsageInfo(response_json)
+
+
+def _displayUsageInfo(response_json):
+    usage = response_json.get("usage", {})
+    completion = usage.get("completion_tokens_details", {})
+    info = (
+        f"\tPrompt tokens: {usage.get("prompt_tokens")}\n"
+        f"\tCompletion tokens: {usage.get("completion_tokens", 0)}\n"
+        f"\t\tReasoning tokens: {completion.get("reasoning_tokens", 0)}\n"
+        f"\t\tAccepted prediction tokens: {completion.get("accepted_prediction_tokens", 0)}\n"
+        f"\t\tRejected Prediction tokens: {completion.get("rejected_prediction_tokens", 0)}\n"
+        f"\tTotal tokens: {usage.get("total_tokens")}"
+    )
+    print(info)
+
+
+def main():
+    os.system("cls")
+    prev = time.time()
+    print("="*50)
+    print("[*] Extracting new context ...")
+    print("="*50)
+    
+    in_lang, out_lang = "hangul", "english"
+    raw_folder_path = ".\\raw"
+    context_file = "context.txt"
+    
+    files = os.listdir(raw_folder_path)
+    chaps = []
+    for chap in files[:]:
+        if ("~$" not in chap) and (chap != ".git"):
+            chaps.append(chap.split(".")[0])
+    
+    sys_role = f"You are an expert reader and context writer of both {in_lang} and {out_lang}."
+    instruction = f"I want you to extract new nouns that don't exist in the {in_lang} text that I will give you by cross-checking the context/dictionary you've previously created."
+    
+    with open(context_file, "r", encoding="utf-8") as file:
+        context = file.read().strip()
+    
+    confirmation = (
+        f"Understood. I will extract only the nouns that doesn't exist in this context/dictionary:\n"
+        f"[{context}]\n"
+        f"Then, I will translate it to {out_lang} and will only respond strictly with this format display, nothing else:\n"
+        "\t{\"hangul1\" – \"english1\". \"hangul2\" – \"english2\" \"hangul3\" – \"english3\".};\n"
+        "However, if there's no new noun, I will just display:\n"
+        "\t{None};\n"
+        "For name (unique/personal identity)  only, add male or female to it. Example:\n"
+        "\t\"hangul3\" – \"english3\", male"
+    )
+    messages = [
+        {"role":"system", "content":sys_role},
+        {"role":"user", "content":instruction},
+        {"role":"assistant", "content":confirmation},
+        {"role":"user", "content":""}
+    ]
+    
+    extracted_context = context
+    for i in chaps:
+        document = Document(os.path.join(raw_folder_path, f"{i}.docx"))
+        text = "\n".join([p.text for p in document.paragraphs])
+        messages[3]["content"] = f"{in_lang.upper()} text:\n\n{text}"
+        new_context = extractContext(messages, i)
+
+        if new_context in ["", None]:
+            print(f"[!] Unsuccessful extraction - {i}")
+            continue
+
+        extracted_context += f"\n\n{new_context}"
+        confirmation = f"{confirmation[:-1]} {new_context}]"
+        messages[2]["content"] = confirmation
+
+        # update context file
+        with open(context_file, "w", encoding="utf-8") as file:
+            file.write(extracted_context)
+        # update context in memory
+        with open(context_file, "r", encoding="utf-8") as file:
+            context = file.read().strip()
+
+    print("="*50)
+    curr = time.time()
+    elapsed = curr - prev
+    print(f"[*] Elapsed time is {elapsed}")
+    print("="*50)
+
+
+if __name__ == "__main__":
+    main()
+    winsound.Beep(1000, 1*1000)
+
+    
--- novel_tlr.py
+++ novel_tlr.py
@@ -1,12 +1,14 @@
+from docx import Document
 import json
 import os
 import requests
 import sys
 import time
-from docx import Document
+import winsound
 
 
 def displayFooter():
+    print("="*50)
     print("[*] Wait for a few moments ...")
     print("="*50)
     time.sleep(3)
@@ -23,9 +25,8 @@ def getFolderPaths():
             continue
         break
     print("[*] Enter absolute folder path to store the translated chapter(s):")
-    tl_folder_path = os.path.abspath(input("\t>> "))
+    tl_folder_path = os.path.abspath(input("\t>> ").strip())
     os.makedirs(tl_folder_path, exist_ok=True)
-    print("="*50)
     displayFooter()
     return raw_folder_path, tl_folder_path
 
@@ -48,17 +49,28 @@ def getAISpecs(specs_file="specs.json"):
         if choice == '1': break
         if choice == '2': specs["in_lang"], specs["out_lang"] = _getLanguages()
         elif choice == '3': specs["instruction"] = _getInstruction()
-        elif choice == '4': specs["context"] = _getContext()
+        elif choice == '4': specs["context"] = _rmNewLine(_getContext())
         elif choice == '5': specs["retries"] = _getNumOfRetries()
-        elif choice == 'q': return
+        elif choice == 'q':
+            print("[*] Exiting program ...")
+            print("="*50)
+            time.sleep(3)
+            sys.exit()
 
     with open(specs_file, "w", encoding="utf-8") as file:
         json.dump(specs, file, indent=4, ensure_ascii=False)
-    
+
+    role = f"You are a professional novel translator of {specs["in_lang"]} to {specs["out_lang"]}."
+    confirmation = (
+        f"Understood. I will strictly follow this instructions: {specs["instruction"]}.\n"
+        f"Then, I will cross-check the {specs["out_lang"]} translation and spelling of the names, "
+        f"genders, titles, skills, and other keywords based from this context: {specs["context"]}."
+    )
     messages = [
-        {"role": "system", "content": f"You are a professional novel translator from {specs["in_lang"]} to {specs["out_lang"]}."},
-        {"role": "user", "content": f"{specs["instruction"]}"},
-        {"role": "assistant", "content": f"Understood. I will cross-check the hangul and their english translation based on this context: {specs["context"]}"}
+        {"role":"system", "content":role},
+        {"role":"user", "content":"Translate something for me."},
+        {"role":"assistant", "content": confirmation},
+        {"role":"user", "content":""}
     ]
     return messages, specs["retries"]
 
@@ -86,11 +98,10 @@ def _getMenuChoice():
 def _getLanguages():
     os.system("cls")
     print("="*50)
-    in_lang = input("[*] Enter input Language: ")
-    out_lang = input("[*] Enter output Language: ")
-    print("="*50)
+    in_lang = input("[*] Enter input Language: ").strip()
+    out_lang = input("[*] Enter output Language: ").strip()
     displayFooter()
-    return '{'+in_lang+'}', '{'+out_lang+'}'
+    return '['+in_lang+']', '['+out_lang+']'
 
 def _getInstruction():
     os.system("cls")
@@ -99,9 +110,8 @@ def _getInstruction():
     print("[*] Press {Enter} >> {Ctrl + Z} >> {Enter} in order once done.")
     print("="*50)
     instruction = sys.stdin.read().strip()
-    print("="*50)
     displayFooter()
-    return '{'+instruction+'}'
+    return '['+instruction+']'
 
 def _getContext():
     os.system("cls")
@@ -110,9 +120,8 @@ def _getContext():
     print("[*] Press {Enter} >> {Ctrl + Z} >> {Enter} in order once done.")
     print("="*50)
     context = sys.stdin.read().strip()
-    print("="*50)
     displayFooter()
-    return '{'+context+'}'
+    return '['+context+']'
 
 def _getNumOfRetries():
     os.system("cls")
@@ -121,55 +130,88 @@ def _getNumOfRetries():
         try:
             print("[*] Enter number of re-tries for translation failure of each chapter.")
             print("[*] Enter '0' if you don't want to re-try any translation failure.")
-            retries = int(float(input("\t>> ")))
+            retries = int(float(input("\t>> "))).strip()
         except ValueError:
             print("[!] Error: Invalid input. Only enter an integer.")
             continue
-        print("="*50)
         displayFooter()
         return retries
+
+def _rmNewLine(text):
+    text_list = text.split("\n")
+    new_text = ""
+    for elem in text_list:
+        if elem == "":
+            continue
+        new_text += elem
+        if elem[-1] != " ":
+            new_text += " "
+    return new_text
 
 
 def getFileNames(raw_folder_path):
     files = os.listdir(raw_folder_path)
     file_names = []
-    for name in files[:]:
-        if ("~$" not in name) and (name != ".git"):
-            file_names.append(name)
+    for chap in files[:]:
+        if ("~$" not in chap) and (chap != ".git"):
+            file_names.append(chap)
     return file_names
 
 
 def getChaps(raw_folder_path, file_names):
     chaps = {}
-    for name in file_names:
-        i = name.index(".")
-        document = Document(f"{raw_folder_path}\\{name}")
+    for chap in file_names:
+        i = chap.index(".")
+        document = Document(f"{raw_folder_path}\\{chap}")
         content = "\n".join([p.text for p in document.paragraphs])
-        chaps[name[:i]] = content
+        chaps[chap[:i]] = content
     return chaps
 
 
-def getTranslation(url, key, model, messages, content):
-    messages_cp = messages[:]
-    messages_cp.append({"role": "user", "content" : f"translate this:\n{content}"})
-    response = requests.post(
-        url=url,
-        headers={
-            "Authorization": f"Bearer {key}",
-            "Content-Type": "application/json",
-        },
-        data=json.dumps({
-            "model": model,
-            "messages": messages_cp
-        })
+def getTranslation(chap, messages, content, retries):
+    url="https://openrouter.ai/api/v1/chat/completions"
+    # key = "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"
+    # model = "deepseek/deepseek-r1:free"
+    key = "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"
+    model="deepseek/deepseek-chat:free"
+
+    messages[3]["content"] = f"translate this:\n{content}"
+    for _ in range(retries + 1):
+        response = requests.post(
+            url=url,
+            headers={
+                "Authorization": f"Bearer {key}",
+                "Content-Type": "application/json",
+            },
+            data=json.dumps({
+                "model": model,
+                "messages": messages
+            })
+        )
+        response_json = response.json()
+        if "choices" in response_json.keys():
+            chap_translation = response_json["choices"][0]["message"]["content"]
+            if "**CHAPTER END**" in chap_translation:
+                print(f"[*] Successful translation - {chap}")
+                _displayUsageInfo(response_json)
+                return chap_translation
+            print(f"[!] Error: Incomplete extraction - {chap} >> Restarting ...")
+        else:
+            print(f"[!] Error: No response - {chap} >> Restarting ...")
+        _displayUsageInfo(response_json["usage"])
+
+def _displayUsageInfo(response_json):
+    usage = response_json.get("usage", {})
+    completion = usage.get("completion_tokens_details", {})
+    info = (
+        f"\tPrompt tokens: {usage.get("prompt_tokens")}\n"
+        f"\tCompletion tokens: {usage.get("completion_tokens", 0)}\n"
+        f"\t\tReasoning tokens: {completion.get("reasoning_tokens", 0)}\n"
+        f"\t\tAccepted prediction tokens: {completion.get("accepted_prediction_tokens", 0)}\n"
+        f"\t\tRejected Prediction tokens: {completion.get("rejected_prediction_tokens", 0)}\n"
+        f"\tTotal tokens: {usage.get("total_tokens")}"
     )
-    response_json = response.json()
-    chap_translation = ""
-    if "choices" not in response_json.keys():
-        return
-    for choice in response_json["choices"]:
-        chap_translation += choice["message"]["content"] + "\n"
-        return chap_translation
+    print(info)
 
 
 def saveChap(text, folder_path, file_name):
@@ -180,12 +222,6 @@ def saveChap(text, folder_path, file_name):
 
 
 def main():
-    url="https://openrouter.ai/api/v1/chat/completions"
-    # key = "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"
-    # model = "deepseek/deepseek-r1:free"
-    key = "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"
-    model="deepseek/deepseek-chat:free"
-
     raw_folder_path, tl_folder_path = getFolderPaths()
     messages, retries = getAISpecs()
     file_names = getFileNames(raw_folder_path)
@@ -207,18 +243,12 @@ def main():
     print("[*] Results")
     print("="*50)
    
-    for name in chaps.keys():
-        chap_translation = getTranslation(url, key, model, messages, chaps[name])
-        for i in range(retries + 1):
-            if (chap_translation not in ["", None]) and ("**CHAPTER END**" in chap_translation):
-                saveChap(chap_translation, tl_folder_path, name)
-                print(f"[*] Successful translation - {name}")
-                break
-            elif (chap_translation in ["", None]):
-                print(f"[!] Error: No response - {name} >> Restarting translation ...")
-            else:
-                print(f"[!] Error: Incomplete translation - {name} >> Restarting translation ...")
-            chap_translation = getTranslation(url, key, model, messages, chaps[name])
+    for chap in chaps.keys():
+        chap_translation = getTranslation(chap, messages, chaps[chap], retries)
+        if chap_translation in ["", None]:
+            print(f"[!] Unsuccessful extraction - {chap}")
+            continue
+        saveChap(chap_translation, tl_folder_path, chap)
   
     curr = time.time()
     elapsed = curr - prev
@@ -227,7 +257,8 @@ def main():
     print(f"[*] Elapsed time is {elapsed}")
     print(f"[*] Output path is '{tl_folder_path}'")
     print("="*50)
-            
 
+            
 if __name__ == "__main__":
     main()
+    winsound.Beep(1000, 1*1000) # hz, ms
